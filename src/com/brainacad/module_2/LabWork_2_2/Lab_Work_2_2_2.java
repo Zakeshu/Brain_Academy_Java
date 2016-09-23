72311b4487b39b6608158bb79ca8345bdc97658c
alexander.zemlyanskiy <alexander.zemlyanskiy@together.com>
2016-09-23 19:53:21 +0300

    some updates

--- src/com/brainacad/module_2/LabWork_2_2/Lab_Work_2_2_2.java
+++
@@ -1,8 +0,0 @@
-package com.brainacad.module_2.LabWork_2_2;
-
-/**
- * Created by a.zemlyanskiy on 05.09.2016.
- * Write class that create instances of class Matrix and execute his methods.
- */
-public class Lab_Work_2_2_2 {
-}

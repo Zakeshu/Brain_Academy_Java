b7e4b8acd0a16339c7ee563567315835eb52b55f
alexander.zemlyanskiy <alexander.zemlyanskiy@together.com>
2016-09-30 12:00:33 +0300

    some updates

--- src/com/brainacad/module_2/LabWork_2_2/Matrix.java
+++ src/com/brainacad/module_2/LabWork_2_2/Matrix.java
@@ -5,6 +5,16 @@ package com.brainacad.module_2.LabWork_2_2;
  * Created by a.zemlyanskiy on 05.09.2016.
  * Create class Matrix with 2D array (int, size 2x2) field and methods: addition and multiplication of matrices;
  */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Таня on 10.09.2016.
+ * Create class Matrix with 2D array (int, size 2x2) field and methods: addition and multiplication of matrices;
+ */
+
 public class  Matrix {
     public static int n = 2;
     public static int count = 0;
@@ -70,44 +80,4 @@ public class  Matrix {
         return matrix_C;
     }
 }
-//
-//---
-//n - размерность матриц
-//int n = 2;
-//    int[][] A,B,C;
-//A = new int[n][n];
-//        B = new int[n][n];
-//        C = new int[n][n];
-//        int i,j,k;
-//        //заполняем рандомными значениями матрицы А и В
-//        System.out.println("Матрица А:");
-//        for(i = 0; i < n; i++){
-//        for(j = 0; j < n; j++){
-//        A[i][j] = (int)(Math.random() * n * 10);
-//        System.out.print(A[i][j] + (j == n - 1?"\n":"\t"));
-//        }
-//        }
-//        System.out.println("Матрица B:");
-//        for(i = 0; i < n; i++){
-//        for(j = 0; j < n; j++){
-//        B[i][j] = (int)(Math.random() * n * 10);
-//        System.out.print(B[i][j] + (j == n - 1?"\n":"\t"));
-//        }
-//        }
-//        //складываем матрицы А и В
-//        System.out.println("Сумма матриц A и B:");
-//        for(i = 0; i < n; i++){
-//        for(j = 0; j < n; j++){
-//        C[i][j] = A[i][j] + B[i][j];
-//        System.out.print(C[i][j] + (j == n - 1?"\n":"\t"));
-//        }
-//        }
-//        //вычитаем матрицу В из матрицы А
-//        System.out.println("Разность матриц A и B:");
-//        for(i = 0; i < n; i++){
-//        for(j = 0; j < n; j++){
-//        C[i][j] = A[i][j] - B[i][j];
-//        System.out.print(C[i][j] + (j == n - 1?"\n":"\t"));
-//        }
-//        }
-//        }
+
